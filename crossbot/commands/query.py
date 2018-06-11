f9f394027c6935602964951d885c0e4e66b4116f
Max Willsey <me@mwillsey.com>
2018-06-11 11:04:12 -0700

    Add more query features

--- crossbot/commands/query.py
+++ crossbot/commands/query.py
@@ -8,7 +8,7 @@ def init(client):
     parser = client.parser.subparsers.add_parser('query', help='Run a saved query')
     parser.set_defaults(command=query)
 
-    parser.add_argument('name', help='Stored query name to run')
+    parser.add_argument('name', nargs='?', help='Stored query name to run')
     parser.add_argument('--save', action='store_true', help='Create or overwrite a stored query')
     parser.add_argument('params', nargs='*', help='Parameters for the stored query or, if saving, the query itself, with question marks for parameters')
 
@@ -22,7 +22,7 @@ def query(client, request):
             '''
             con.execute(query, (request.args.name, cmd, request.userid, datetime.now()))
         request.reply("Saved new query `{}` from {}".format(request.args.name, client.user(request.userid)))
-    else:
+    elif request.args.name:
         params = [sql.format_sql_cmd(param) for param in request.args.params]
         with sqlite3.connect(crossbot.db_path) as con:
             query = '''
@@ -43,3 +43,24 @@ def query(client, request):
             request.reply(result)
         else:
             request.reply("No known command `{}`".format(request.args.name))
+    else:
+        with sqlite3.connect(crossbot.db_path) as con:
+            queries = con.execute('''
+            SELECT name, userid, timestamp, command FROM query_shorthands
+            ''').fetchall()
+
+        msgs = []
+        for (name, userid, timestamp, command) in queries:
+            n_args = command.count('?')
+            if n_args:
+                maybe_s = '' if n_args == 1 else 's'
+                args = ' (takes {} arg{})'.format(n_args, maybe_s)
+            else:
+                args = ''
+            msg = '*{}* by {}{}:\n {}'.format(name, client.user(userid), args, command)
+            msgs.append(msg)
+
+        if msgs:
+            request.reply('\n\n'.join(msgs))
+        else:
+            request.reply('There are no saved messages yet... make one with `query --save ...`!')
