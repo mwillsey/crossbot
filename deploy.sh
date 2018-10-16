a9cf142ec2e04c39ec443c64838aa7bf586a1c2c
Max Willsey <me@mwillsey.com>
2018-10-16 07:29:57 -0700

    Make backups before deploying

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+set -e
+
+backup="/tmp/crossbot.db.$(date -Iseconds)"
+scp uwplse.org:/var/www/crossbot/crossbot.db $backup
+echo "Backed up to $backup"
 rsync -rz --delete --itemize-changes --progress --exclude-from .gitignore --exclude .git . uwplse.org:/var/www/crossbot
