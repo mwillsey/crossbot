8b6ac8afc25b7645ea75294b3833a0531e19160d
Max Willsey <me@mwillsey.com>
2018-10-16 07:30:25 -0700

    Use media directory

--- settings.py
+++ settings.py
@@ -34,6 +34,9 @@ DEBUG = bool(int(os.environ.get('DJANGO_DEBUG', True)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 ALLOWED_HOSTS = [
     'localhost',
     'mwillsey.com',
