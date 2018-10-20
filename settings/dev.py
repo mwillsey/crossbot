636d5fdc0def8a3e70563162f3ec3cd4b3559d07
Max Willsey <me@mwillsey.com>
2018-10-20 08:27:34 -0700

    Add a fake token to dev

--- settings/dev.py
+++ settings/dev.py
@@ -7,6 +7,7 @@ SECRET_KEY = 'my_django_secret_key'
 # slack stuff
 SLACK_SECRET_SIGNING_KEY = b'my_secret_slack_key'
 SLACK_OAUTH_ACCESS_TOKEN = 'my_slack_oauth'
+SLACK_OAUTH_BOT_ACCESS_TOKEN = 'my_slack_bot_oauth'
 
 # Slack OAuth stuff
 SOCIAL_AUTH_SLACK_TEAM = 'slack-team-id'
