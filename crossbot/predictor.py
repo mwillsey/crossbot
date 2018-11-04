f4320844dd7015a1b164add11d7c5c29dd88990c
Pavel Panchekha <me@pavpanchekha.com>
2018-11-04 12:16:35 -0800

    Suppress printing in the predictor

--- crossbot/predictor.py
+++ crossbot/predictor.py
@@ -61,6 +61,37 @@ def munge_data(uids, dates, dows, secs, ts):
     }
 
 
+class suppress_stdout_stderr(object):
+    '''
+    A context manager for doing a "deep suppression" of stdout and stderr in
+    Python, i.e. will suppress all print, even if the print originates in a
+    compiled C/Fortran sub-function.
+       This will not suppress raised exceptions, since exceptions are printed
+    to stderr just before a script exits, and after the context manager has
+    exited (at least, I think that is why it lets exceptions through).
+
+    '''
+
+    def __init__(self):
+        # Open a pair of null files
+        self.null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
+        # Save the actual stdout (1) and stderr (2) file descriptors.
+        self.save_fds = [os.dup(1), os.dup(2)]
+
+    def __enter__(self):
+        # Assign the null pointers to stdout and stderr.
+        os.dup2(self.null_fds[0], 1)
+        os.dup2(self.null_fds[1], 2)
+
+    def __exit__(self, *_):
+        # Re-assign the real stdout/stderr back to (1) and (2)
+        os.dup2(self.save_fds[0], 1)
+        os.dup2(self.save_fds[1], 2)
+        # Close the null files
+        for fd in self.null_fds + self.save_fds:
+            os.close(fd)
+
+
 def fit(data):
     path = os.path.join(os.path.dirname(__file__), 'predictor.stan')
     with open(path, "r") as f:
@@ -83,16 +114,17 @@ def fit(data):
         with open(cache_path, "wb") as f:
             pickle.dump(sm, f)
 
-    fm = sm.sampling(
-        data=munge_data(**data),
-        iter=1000,
-        chains=4,
-        pars=[
-            'date_effect', 'skill_effect', 'predictions', 'residuals',
-            'beginner_gain', 'beginner_decay', 'sat_effect', 'mu', 'skill_dev',
-            'date_dev', 'sigma'
-        ]
-    )
+    with suppress_stdout_stderr():
+        fm = sm.sampling(
+            data=munge_data(**data),
+            iter=1000,
+            chains=4,
+            pars=[
+                'date_effect', 'skill_effect', 'predictions', 'residuals',
+                'beginner_gain', 'beginner_decay', 'sat_effect', 'mu',
+                'skill_dev', 'date_dev', 'sigma'
+            ],
+        )
     return fm
 
 
